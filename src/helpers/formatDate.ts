c234f3f5fba098a71a808a7fce3e100954e59cc2
Mauro <mauromaio.dev@gmail.com>
2023-11-24 18:08:27 -0300

    feat: formatDate helper

---
+++ src/helpers/formatDate.ts
@@ -0,0 +1,12 @@
+export const formatDate = (rawDate: string): string => {
+  const dateObject = new Date(rawDate)
+
+  const dayOfWeek = dateObject.toLocaleString('pt-BR', { weekday: 'long' })
+  const day = dateObject.getDate()
+  const month = dateObject.toLocaleString('pt-BR', { month: 'short' })
+
+  const formattedDayOfWeek = dayOfWeek.charAt(0).toUpperCase() + dayOfWeek.slice(1).toLowerCase()
+  const formattedMonth = month.charAt(0).toUpperCase() + month.slice(1).toLowerCase()
+
+  return `${formattedDayOfWeek}, ${day} ${formattedMonth}.`
+}
