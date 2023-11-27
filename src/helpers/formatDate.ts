f5eddd371c75c2544dfc9829fbc4f44afb996390
Mauro <mauromaio.dev@gmail.com>
2023-11-27 12:43:09 -0300

    feat: formatDate adjustments prototype

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,9 +1,13 @@
 export const formatDate = (rawDate: string): string => {
-  const dateObject = new Date(rawDate)
+  const dateObject = new Date(rawDate + 'T00:00:00')
 
-  const dayOfWeek = dateObject.toLocaleString('pt-BR', { weekday: 'long' })
+  const dayOfWeek = dateObject.toLocaleString('pt-BR', {
+    weekday: 'long'
+  })
   const day = dateObject.getDate()
-  const month = dateObject.toLocaleString('pt-BR', { month: 'short' })
+  const month = dateObject.toLocaleString('pt-BR', {
+    month: 'short'
+  })
 
   const formattedDayOfWeek = dayOfWeek.charAt(0).toUpperCase() + dayOfWeek.slice(1).toLowerCase()
   const formattedMonth = month.charAt(0).toUpperCase() + month.slice(1).toLowerCase()
